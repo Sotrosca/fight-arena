31770b51fe68c38c8a40b137eef3bdd364599e11
Facundo Serna <41481961+Sotrosca@users.noreply.github.com>
2024-02-20 18:23:48 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,8 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="fight-arena",
-    version="0.0.
-    1",
+    version="0.0.1",
     packages=find_packages(),
     install_requires=[
         "certifi==2023.11.17",
