90cc3efbd732da82d910bc5fc09639ae4b4b4bd9
Facundo Serna <facuserna93@gmail.com>
2024-02-22 20:52:47 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@ from setuptools import find_packages, setup
 setup(
     name="fight_arena",
     version="0.4",
-    packages=["fight_arena"],
+    packages=find_packages(),
+    py_modules=["game"],
     install_requires=["pygame==2.5.2"],
 )
