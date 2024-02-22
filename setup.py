cb7c1ba5b2041b41e575796922780a6f2ce9287f
Facundo Serna <facuserna93@gmail.com>
2024-02-22 19:43:49 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="fight_arena",
-    version="0.2",
-    packages=find_packages(),
+    version="0.3",
+    packages=["inuyasha"],
     install_requires=[],
 )
