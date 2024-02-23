65fb363b0e0c0c3eda7af372942171b9470149c9
Facundo Serna <facuserna93@gmail.com>
2024-02-22 21:30:53 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,9 +4,9 @@ from setuptools import find_packages, setup
 
 setup(
     name="fight_arena",
-    version="0.15",
+    version="0.16",
     packages=find_packages(),
     py_modules=["game", "constants", "fighter", "layers", "components"],
-    data_files=[("assets", glob.glob("assets/*.jpg"))],
+    package_data={"": ["assets/*.jpg"]},
     install_requires=["pygame==2.5.2"],
 )
